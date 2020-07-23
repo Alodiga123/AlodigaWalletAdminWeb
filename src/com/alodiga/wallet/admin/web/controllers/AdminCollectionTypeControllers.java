1fc23d9026bd2a8a51be9b4a61794420678703bb
adiquintero <yquintero@alodiga.com>
2020-07-23 19:03:09 -0400

    crud recaudos

---
+++ src/com/alodiga/wallet/admin/web/controllers/AdminCollectionTypeControllers.java
@@ -0,0 +1,175 @@
+package com.alodiga.wallet.admin.web.controllers;
+
+
+import com.alodiga.wallet.admin.web.generic.controllers.GenericAbstractAdminController;
+import com.alodiga.wallet.admin.web.utils.WebConstants;
+import com.alodiga.wallet.common.ejb.UtilsEJB;
+import com.alodiga.wallet.common.exception.EmptyListException;
+import com.alodiga.wallet.common.exception.GeneralException;
+import com.alodiga.wallet.common.exception.NullParameterException;
+import com.alodiga.wallet.common.genericEJB.EJBRequest;
+import com.alodiga.wallet.common.model.CollectionType;
+import com.alodiga.wallet.common.model.Country;
+import com.alodiga.wallet.common.utils.EJBServiceLocator;
+import com.alodiga.wallet.common.utils.EjbConstants;
+import java.util.List;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Toolbarbutton;
+
+public class AdminCollectionTypeControllers extends GenericAbstractAdminController {
+
+    private static final long serialVersionUID = -9145887024839938515L;
+    private Textbox txtDescription;
+    private Combobox cmbCountry;
+    private UtilsEJB utilsEJB = null;
+    private CollectionType collectionTypeParam;
+    private Toolbarbutton tbbTitle;
+    private Button btnSave;
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        eventType = (Integer) Sessions.getCurrent().getAttribute(WebConstants.EVENTYPE);
+        if (eventType == WebConstants.EVENT_ADD) {
+            collectionTypeParam = null;
+        } else {
+            collectionTypeParam = (CollectionType) Sessions.getCurrent().getAttribute("object");
+        }
+        initialize();
+        loadData();
+    }
+
+    
+    @Override
+    public void initialize() {
+        super.initialize();
+        switch (eventType) {
+            case WebConstants.EVENT_EDIT:
+                tbbTitle.setLabel(Labels.getLabel("cms.common.collectionsType.edit"));
+                break;
+            case WebConstants.EVENT_VIEW:
+                tbbTitle.setLabel(Labels.getLabel("cms.common.collectionsType.view"));
+                break;
+            case WebConstants.EVENT_ADD:
+                tbbTitle.setLabel(Labels.getLabel("cms.common.collectionsType.add"));
+                break;
+            default:
+                break;
+        }
+        try {
+            utilsEJB = (UtilsEJB) EJBServiceLocator.getInstance().get(EjbConstants.UTILS_EJB);
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void clearFields() {
+        txtDescription.setRawValue(null);
+    }
+    
+     public Boolean validateEmpty() {
+        if (txtDescription.getText().isEmpty()) {
+            txtDescription.setFocus(true);
+            this.showMessage("sp.error.field.cannotNull", true, null);
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    private void loadFields(CollectionType collectionType) {
+         try {
+            txtDescription.setText(collectionType.getDescription());
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void blockFields() {
+        txtDescription.setReadonly(true);
+        btnSave.setVisible(false);
+    }
+
+    private void saveCollections(CollectionType collectionType_) {
+        try {
+            CollectionType collectionType = null;
+
+            if (collectionType_ != null) {
+                collectionType = collectionType_;
+            } else {//New requestType
+                collectionType = new CollectionType();
+            }
+
+            //insertando en collectionType
+            collectionType.setCountryId((Country) cmbCountry.getSelectedItem().getValue());
+            collectionType.setDescription(txtDescription.getText());
+            collectionType = utilsEJB.saveCollectionType(collectionType);
+            collectionTypeParam = collectionType;
+            this.showMessage("sp.common.save.success", false, null);
+            btnSave.setDisabled(true);
+        } catch (Exception ex) {
+            showError(ex);
+        }
+
+    }
+
+    public void onClick$btnSave() {
+        if (validateEmpty()) {
+            switch (eventType) {
+                case WebConstants.EVENT_ADD:
+                    saveCollections(null);
+                    break;
+                case WebConstants.EVENT_EDIT:
+                    saveCollections(collectionTypeParam);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void loadData() {
+        switch (eventType) {
+            case WebConstants.EVENT_EDIT:
+                loadFields(collectionTypeParam);
+                loadCmbCountry(eventType);
+                break;
+            case WebConstants.EVENT_VIEW:
+                loadFields(collectionTypeParam);
+                blockFields();
+                loadCmbCountry(eventType);
+                break;
+            case WebConstants.EVENT_ADD:
+                loadCmbCountry(eventType);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loadCmbCountry(Integer evenInteger) {
+        //cmbCountry
+        EJBRequest request1 = new EJBRequest();
+        List<Country> countries;
+
+        try {
+            countries = utilsEJB.getCountries(request1);
+            loadGenericCombobox(countries, cmbCountry, "name", evenInteger, Long.valueOf(collectionTypeParam != null ? collectionTypeParam.getCountryId().getId() : 0));
+        } catch (EmptyListException ex) {
+            showError(ex);
+            ex.printStackTrace();
+        } catch (GeneralException ex) {
+            showError(ex);
+            ex.printStackTrace();
+        } catch (NullParameterException ex) {
+            showError(ex);
+            ex.printStackTrace();
+        }
+    }
+
+}
