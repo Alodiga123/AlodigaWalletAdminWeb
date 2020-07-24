bd2e351872e49da2aa5ea9472a68970be147a4d1
Unknown <lgutierrez@alodiga.com>
2020-07-24 11:36:11 -0400

    opcion nueva en el menu LIST_BUSINESS_CATEGORY

--- src/com/alodiga/wallet/admin/web/controllers/AdminMainMenuController.java
+++ src/com/alodiga/wallet/admin/web/controllers/AdminMainMenuController.java
@@ -168,6 +168,7 @@ public class AdminMainMenuController extends GenericForwardComposer {
         createCell(Permission.LIST_EXCHANGE_RATE, "listExchangeRate.zul", permissionGroup, listgroup);
         createCell(Permission.LIST_COMMISSION, "listCommission.zul", permissionGroup, listgroup);
         createCell(Permission.LIST_DOCUMENTS_PERSON_TYPE, "listDocumentsPersonType.zul", permissionGroup, listgroup);
+        createCell(Permission.LIST_BUSINESS_CATEGORY, "listBusinnesCategory.zul", permissionGroup, listgroup);
     }
 
     private void loadSecurityManagementGroup(PermissionGroup permissionGroup) {
