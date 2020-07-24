90782797eb762c9d4e4f5fecb030ad2ce9656456
Unknown <lgutierrez@alodiga.com>
2020-07-24 11:36:52 -0400

    controladores de categorias de comercio NUEVOS

---
+++ src/com/alodiga/wallet/admin/web/controllers/AdminBusinnesSubCategoryController.java
@@ -0,0 +1,201 @@
+package com.alodiga.wallet.admin.web.controllers;
+
+import com.alodiga.wallet.admin.web.generic.controllers.GenericAbstractAdminController;
+import com.alodiga.wallet.admin.web.utils.WebConstants;
+import com.alodiga.wallet.common.ejb.UtilsEJB;
+import com.alodiga.wallet.common.exception.EmptyListException;
+import com.alodiga.wallet.common.exception.GeneralException;
+import com.alodiga.wallet.common.exception.NullParameterException;
+import com.alodiga.wallet.common.utils.EJBServiceLocator;
+import com.alodiga.wallet.common.utils.EjbConstants;
+import java.util.List;
+import com.alodiga.wallet.common.genericEJB.EJBRequest;
+import com.alodiga.wallet.common.model.BusinessCategory;
+import com.alodiga.wallet.common.model.BusinessSubCategory;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventQueues;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+
+public class AdminBusinnesSubCategoryController extends GenericAbstractAdminController {
+
+    private static final long serialVersionUID = -9145887024839938515L;
+    private Combobox cmbBusinnessCategory;
+    private Label lblBusinnessCategory;
+    private Textbox txtDescription;
+    private Textbox txtMccCode;
+    private UtilsEJB utilsEJB = null;
+    private Button btnSave;
+    private BusinessSubCategory businessSubCategoryParam;
+    private Integer eventType;
+    public Window winAdminBusinnesSubCategory;
+    private BusinessCategory businessCategory;
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        eventType = (Integer) Sessions.getCurrent().getAttribute(WebConstants.EVENTYPE);
+        if (eventType == WebConstants.EVENT_ADD) {
+            businessSubCategoryParam = null;
+        } else {
+            businessSubCategoryParam = (BusinessSubCategory) Sessions.getCurrent().getAttribute("object");
+        }
+        initialize();
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        try {
+            utilsEJB = (UtilsEJB) EJBServiceLocator.getInstance().get(EjbConstants.UTILS_EJB);
+            loadData();
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void clearFields() {
+        txtDescription.setRawValue(null);
+        txtMccCode.setRawValue(null);
+    }
+
+    private void loadFields(BusinessSubCategory businessSubCategory) {
+        try {
+
+            AdminBusinnessCategoryController adminBusinness = new AdminBusinnessCategoryController();
+            businessCategory = adminBusinness.getBusinessCategory();
+
+            if (adminBusinness.getBusinessCategory() != null) {
+                lblBusinnessCategory.setValue(businessCategory.getDescription());
+            } else {
+                lblBusinnessCategory.setValue(businessSubCategory.getBusinessCategoryId().getDescription());
+            }
+            lblBusinnessCategory.setValue(businessCategory.getDescription());
+            txtDescription.setValue(businessSubCategory.getDescription());
+            txtMccCode.setValue(businessSubCategory.getMccCode());
+
+            btnSave.setVisible(true);
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    private void loadFieldCategory() {
+        try {
+            AdminBusinnessCategoryController adminBusinness = new AdminBusinnessCategoryController();
+            businessCategory = adminBusinness.getBusinessCategory();
+            
+            lblBusinnessCategory.setValue(businessCategory.getDescription());
+            
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void blockFields() {
+        txtDescription.setDisabled(true);
+        txtMccCode.setDisabled(true);
+
+        btnSave.setVisible(false);
+    }
+
+    public boolean validateEmpty() {
+        if (txtDescription.getText().isEmpty()) {
+            txtDescription.setFocus(true);
+            this.showMessage("sp.error.businessCategory.description", true, null);
+        } else if (txtMccCode.getText().isEmpty()) {
+            txtMccCode.setFocus(true);
+            this.showMessage("sp.error.businessCategory.mccCode", true, null);
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    private void saveBusinessSubCategory(BusinessSubCategory _businessSubCategory) {
+        BusinessSubCategory businessSubCategory = null;
+
+        try {
+            if (_businessSubCategory != null) {
+                businessSubCategory = _businessSubCategory;
+            } else {//New country
+                businessSubCategory = new BusinessSubCategory();
+            }
+
+            businessSubCategory.setDescription(txtDescription.getText());
+            businessSubCategory.setMccCode(txtMccCode.getText());
+            businessSubCategory.setBusinessCategoryId(businessCategory);
+            businessSubCategory = utilsEJB.saveBusinessSubCategory(businessSubCategory);
+            businessSubCategoryParam = businessSubCategory;
+            this.showMessage("sp.common.save.success", false, null);
+
+            if (eventType == WebConstants.EVENT_ADD) {
+                btnSave.setVisible(false);
+            } else {
+                btnSave.setVisible(true);
+            }
+            EventQueues.lookup("updateBusinessSubCategory", EventQueues.APPLICATION, true).publish(new Event(""));
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void onClick$btnSave() {
+        if (validateEmpty()) {
+            switch (eventType) {
+                case WebConstants.EVENT_ADD:
+                    saveBusinessSubCategory(businessSubCategoryParam);
+                    break;
+                case WebConstants.EVENT_EDIT:
+                    saveBusinessSubCategory(businessSubCategoryParam);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void onClick$btnBack() {
+        winAdminBusinnesSubCategory.detach();
+    }
+
+    public void loadData() {
+        switch (eventType) {
+            case WebConstants.EVENT_EDIT:
+                loadFields(businessSubCategoryParam);
+                break;
+            case WebConstants.EVENT_VIEW:
+                loadFields(businessSubCategoryParam);
+                blockFields();
+                break;
+            case WebConstants.EVENT_ADD:
+                loadFieldCategory();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void loadCmbBusinnesCategory(Integer evenInteger) {
+        EJBRequest request1 = new EJBRequest();
+        List<BusinessCategory> businessCategories;
+        try {
+            businessCategories = utilsEJB.getBusinessCategory(request1);
+            loadGenericCombobox(businessCategories, cmbBusinnessCategory, "description", evenInteger, Long.valueOf(businessSubCategoryParam != null ? businessSubCategoryParam.getBusinessCategoryId().getId() : 0));
+        } catch (EmptyListException ex) {
+            showError(ex);
+            ex.printStackTrace();
+        } catch (GeneralException ex) {
+            showError(ex);
+            ex.printStackTrace();
+        } catch (NullParameterException ex) {
+            showError(ex);
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/com/alodiga/wallet/admin/web/controllers/ListBusinnesSubCategoryController.java
@@ -0,0 +1,263 @@
+package com.alodiga.wallet.admin.web.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listcell;
+import org.zkoss.zul.Listitem;
+import com.alodiga.wallet.admin.web.generic.controllers.GenericAbstractListController;
+import com.alodiga.wallet.admin.web.utils.AccessControl;
+import com.alodiga.wallet.admin.web.utils.Utils;
+import com.alodiga.wallet.admin.web.utils.WebConstants;
+import com.alodiga.wallet.common.ejb.PersonEJB;
+import com.alodiga.wallet.common.ejb.UtilsEJB;
+import com.alodiga.wallet.common.exception.EmptyListException;
+import com.alodiga.wallet.common.exception.GeneralException;
+import com.alodiga.wallet.common.exception.NullParameterException;
+import com.alodiga.wallet.common.genericEJB.EJBRequest;
+import com.alodiga.wallet.common.manager.PermissionManager;
+import com.alodiga.wallet.common.model.BusinessCategory;
+import com.alodiga.wallet.common.model.BusinessSubCategory;
+import com.alodiga.wallet.common.model.Permission;
+import com.alodiga.wallet.common.model.Profile;
+import com.alodiga.wallet.common.model.User;
+import com.alodiga.wallet.common.utils.Constants;
+import com.alodiga.wallet.common.utils.EJBServiceLocator;
+import com.alodiga.wallet.common.utils.EjbConstants;
+import java.util.HashMap;
+import java.util.Map;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.EventQueue;
+import org.zkoss.zk.ui.event.EventQueues;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+
+public class ListBusinnesSubCategoryController extends GenericAbstractListController<BusinessSubCategory> {
+
+    private static final long serialVersionUID = -9145887024839938515L;
+    private Listbox lbxRecords;
+    private Textbox txtName;
+    private PersonEJB personEJB = null;
+    private UtilsEJB utilsEJB = null;
+    private List<BusinessSubCategory> businessSubCategoryList = null;
+    private User currentUser;
+    private Profile currentProfile;
+    private BusinessCategory businessCategoryParam;
+
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+
+        if (eventType == WebConstants.EVENT_ADD) {
+            businessCategoryParam = null;
+        } else {
+            businessCategoryParam = (Sessions.getCurrent().getAttribute("object") != null) ? (BusinessCategory) Sessions.getCurrent().getAttribute("object") : null;
+        }
+
+        initialize();
+    }
+
+    @Override
+    public void checkPermissions() {
+        try {
+            btnAdd.setVisible(PermissionManager.getInstance().hasPermisssion(currentProfile.getId(), Permission.ADD_BUSINESS_SUB_CATEGORY));
+            permissionEdit = PermissionManager.getInstance().hasPermisssion(currentProfile.getId(), Permission.ADD_BUSINESS_SUB_CATEGORY);
+            permissionRead = PermissionManager.getInstance().hasPermisssion(currentProfile.getId(), Permission.ADD_BUSINESS_SUB_CATEGORY);
+        } catch (Exception ex) {
+            showError(ex);
+        }
+
+    }
+
+    public void startListener() {
+        EventQueue que = EventQueues.lookup("updateBusinessSubCategory", EventQueues.APPLICATION, true);
+        que.subscribe(new EventListener() {
+
+            public void onEvent(Event evt) {
+                getData();
+                loadDataList(businessSubCategoryList);
+            }
+        });
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        try {
+            currentUser = AccessControl.loadCurrentUser();
+            currentProfile = currentUser.getCurrentProfile();
+            checkPermissions();
+            adminPage = "adminBusinnessSubCategory.zul";
+            personEJB = (PersonEJB) EJBServiceLocator.getInstance().get(EjbConstants.PERSON_EJB);
+            utilsEJB = (UtilsEJB) EJBServiceLocator.getInstance().get(EjbConstants.UTILS_EJB);
+            startListener();
+            getData();
+            loadDataList(businessSubCategoryList);
+        } catch (Exception ex) {
+//            showError(ex);
+            showEmptyList();
+        }
+    }
+
+    public void onClick$btnAdd() throws InterruptedException {
+        try {
+            Sessions.getCurrent().setAttribute(WebConstants.EVENTYPE, WebConstants.EVENT_ADD);
+            Map<String, Object> paramsPass = new HashMap<String, Object>();
+            paramsPass.put("object", businessSubCategoryList);
+            final Window window = (Window) Executions.createComponents(adminPage, null, paramsPass);
+            window.doModal();
+        } catch (Exception ex) {
+            this.showMessage("sp.error.general", true, ex);
+        }
+    }
+
+    public void getData() {
+        businessSubCategoryList = new ArrayList<BusinessSubCategory>();
+        try {
+            if (businessCategoryParam != null) {
+                EJBRequest request1 = new EJBRequest();
+                Map params = new HashMap();
+                params.put(Constants.BUSINESS_CATEGORY_ID, businessCategoryParam.getId());
+                request1.setParams(params);
+                businessSubCategoryList = utilsEJB.getBusinessSubCategoryByCategory(request1);
+            } else {
+                showEmptyList();
+            }
+
+        } catch (NullParameterException ex) {
+            showError(ex);
+        } catch (EmptyListException ex) {
+            showEmptyList();
+        } catch (GeneralException ex) {
+            showError(ex);
+        }
+    }
+
+    private void showEmptyList() {
+        Listitem item = new Listitem();
+        item.appendChild(new Listcell(Labels.getLabel("sp.error.empty.list")));
+        item.appendChild(new Listcell());
+        item.appendChild(new Listcell());
+        item.appendChild(new Listcell());
+        item.setParent(lbxRecords);
+    }
+
+    public void loadDataList(List<BusinessSubCategory> list) {
+        try {
+            lbxRecords.getItems().clear();
+            Listitem item = null;
+            if (list != null && !list.isEmpty()) {
+                btnDownload.setVisible(true);
+                for (BusinessSubCategory businessSubCategory : list) {
+                    item = new Listitem();
+                    item.setValue(businessSubCategoryList);
+                    item.appendChild(new Listcell(businessSubCategory.getBusinessCategoryId().getDescription()));
+                    item.appendChild(new Listcell(businessSubCategory.getDescription()));
+                    item.appendChild(createButtonEditModal(businessSubCategory));
+                    item.appendChild(createButtonViewModal(businessSubCategory));
+                    item.setParent(lbxRecords);
+                }
+            } else {
+                btnDownload.setVisible(false);
+                item = new Listitem();
+                item.appendChild(new Listcell(Labels.getLabel("sp.error.empty.list")));
+                item.appendChild(new Listcell());
+                item.appendChild(new Listcell());
+                item.appendChild(new Listcell());
+                item.setParent(lbxRecords);
+            }
+
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public Listcell createButtonEditModal(final Object obg) {
+        Listcell listcellEditModal = new Listcell();
+        try {
+            Button button = new Button();
+            button.setImage("/images/icon-edit.png");
+            button.setTooltiptext(Labels.getLabel("sp.common.actions.edit"));
+            button.setClass("open orange");
+            button.addEventListener("onClick", new EventListener() {
+                @Override
+                public void onEvent(Event arg0) throws Exception {
+                    Sessions.getCurrent().setAttribute("object", obg);
+                    Sessions.getCurrent().setAttribute(WebConstants.EVENTYPE, WebConstants.EVENT_EDIT);
+                    Map<String, Object> paramsPass = new HashMap<String, Object>();
+                    paramsPass.put("object", obg);
+                    final Window window = (Window) Executions.createComponents(adminPage, null, paramsPass);
+                    window.doModal();
+                }
+
+            });
+            button.setParent(listcellEditModal);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return listcellEditModal;
+    }
+
+    public Listcell createButtonViewModal(final Object obg) {
+        Listcell listcellViewModal = new Listcell();
+        try {
+            Button button = new Button();
+            button.setImage("/images/icon-invoice.png");
+            button.setTooltiptext(Labels.getLabel("sp.common.actions.view"));
+            button.setClass("open orange");
+            button.addEventListener("onClick", new EventListener() {
+                @Override
+                public void onEvent(Event arg0) throws Exception {
+                    Sessions.getCurrent().setAttribute("object", obg);
+                    Sessions.getCurrent().setAttribute(WebConstants.EVENTYPE, WebConstants.EVENT_VIEW);
+                    Map<String, Object> paramsPass = new HashMap<String, Object>();
+                    paramsPass.put("object", obg);
+                    final Window window = (Window) Executions.createComponents(adminPage, null, paramsPass);
+                    window.doModal();
+                }
+
+            });
+            button.setParent(listcellViewModal);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return listcellViewModal;
+    }
+
+    public void onClick$btnDownload() throws InterruptedException {
+        try {
+            Utils.exportExcel(lbxRecords, Labels.getLabel("sp.crud.businnesSubCategory.list"));
+            AccessControl.saveAction(Permission.LIST_BUSINESS_SUB_CATEGORY, "Se descargo Sub Categoria de Comercios en formato excel");
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    public void onClick$btnClear() throws InterruptedException {
+        txtName.setText("");
+    }
+
+    public void onClick$btnSearch() throws InterruptedException {
+        try {
+//            loadDataList(getFilterList(txtName.getText()));
+        } catch (Exception ex) {
+            showError(ex);
+        }
+    }
+
+    @Override
+    public List<BusinessSubCategory> getFilteredList(String filter) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void loadList(List<BusinessSubCategory> list) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
